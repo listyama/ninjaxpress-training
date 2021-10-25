4f914d97df7e8bce05a2cd9f6c761aab5274b734
unknown <listya.maharani@ninjavan.co>
2021-10-25 15:00:57 +0700

    Simpan semua file pert-1

---
+++ Pert-1.py
@@ -0,0 +1,24 @@
+print("Hello, Listya!")
+
+#coba yang lain
+
+#membuat variabel
+x = 5
+y = "Hello World!"
+
+print(x)
+print(y)
+
+
+var1 = 5 #integer (int)
+var2 = "susunan kalimat" #string (str)
+var3 = 0.75 #float
+var4 = True #boolean (bool)
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
+
+n = 6/3
+print(n)
