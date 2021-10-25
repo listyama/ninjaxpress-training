a7bb90c666c0a8e2579a1b39a391c0ff816c0b31
unknown <listya.maharani@ninjavan.co>
2021-10-25 16:25:32 +0700

    Simpan semua file pert-2

---
+++ Pert-2.py
@@ -0,0 +1,61 @@
+# name = input("Enter Name:")
+# print ("Your name is " + name)
+
+#OPERASI TERHADAP TIPE DATA (STRING)
+# x = "Hello World!"
+
+# print(len(x))
+# print(x[1:4]) #character 2-4
+# print(x[0:5]) #character 1-5
+
+#CARA REPLACE
+# x = x.replace("World","friend")
+# print(x)
+
+# a = "Hello"
+# b = "World!"
+# c = a + " " + b
+# print(c)
+
+# a = "Siap"
+# b = 86
+# c = a + " " + str(b)
+# print(c)
+
+#TEMPLATE PAKE FORMAT NAMA DAN ALAMAT
+# nama = "listya"
+# alamat = "jakarta"
+
+# my_profile = "Nama saya {} dan saat ini saya bekerja di {}." .format(nama,alamat)
+# print(my_profile)
+
+# x = "Indonesia AI"
+# print(x[2:4])
+# print(len(x))
+
+#BOOLEANS
+# print(10 > 9)
+# print(10 == 9)
+# print(10 < 9)
+
+# a = 10
+# b = 9
+
+# if b > a:
+#     print("b is greater than a")
+# else:
+#     print("b is not greater than b")
+
+#LIST
+my_list = []
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+my_list.append("my_list")
+my_list.append(10 > 9)
+
+print(my_list)
+
+#PYTHON CONDITIONS (IF ELSE)
+a = 200
+b = 33
