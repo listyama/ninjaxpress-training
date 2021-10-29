adbf9070203bda335a041ae84acd819dab831beb
unknown <listya.maharani@ninjavan.co>
2021-10-29 16:34:51 +0700

    Simpan Pertemuan 4

--- 4_function_example.py
+++ 4_function_example.py
@@ -1,21 +1,38 @@
 nama_buah = []
+def tambah_nama_buah(nama):
+    nama_buah.append(nama)
+    print_nama_buah()
 
-nama_buah.append("apel")
-for buah in nama_buah:
+def print_nama_buah():
+    for buah in nama_buah:
         print(buah)
-        print("---")
+    print("---")   
 
-nama_buah.append("jeruk")
-for buah in nama_buah:
-        print(buah)
-        print("---")
+tambah_nama_buah("apel")
+tambah_nama_buah("jeruk")
+tambah_nama_buah("mangga")
 
-nama_buah.append("mangga")
-for buah in nama_buah:
-        print(buah)
-        print("---")
+
+#VERSI AWAL YG GA RINGKAS
+# nama_buah.append("apel")
+# for buah in nama_buah:
+#         print(buah)
+#         print("---")
+
+# nama_buah.append("jeruk")
+# for buah in nama_buah:
+#         print(buah)
+#         print("---")
+
+# nama_buah.append("mangga")
+# for buah in nama_buah:
+#         print(buah)
+#         print("---")
         
-nama_buah.append("pisang")
-for buah in nama_buah:
-        print(buah)
-        print("---")
+# nama_buah.append("pisang")
+# for buah in nama_buah:
+#         print(buah)
+#         print("---")
+
+
+
---
+++ 4_function_return.py
@@ -0,0 +1,9 @@
+def total(x,y):
+    return x + y
+
+print(total(10,15))
+
+buah_sisa = 20
+
+print(total(10, 5) + buah_sisa)
+print(10 + 5 + 20)
