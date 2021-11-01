84bd0f81379241b1bd51ae96161758d76f83ea88
unknown <listya.maharani@ninjavan.co>
2021-11-01 14:56:37 +0700

    pertemuan 5

--- 4_function_return.py
+++ 4_function_return.py
@@ -1,9 +1,19 @@
-def total(x,y):
-    return x + y
+#FUNCTION RETURN VALUE -> ngembaliin nilai
+# def total(x,y):
+#     return x + y
 
-print(total(10,15))
+# print(total(10,15))
 
-buah_sisa = 20
+# buah_sisa = 20
 
-print(total(10, 5) + buah_sisa)
-print(10 + 5 + 20)
+# print(total(10, 5) + buah_sisa)
+# print(10 + 5 + 20)
+
+#QUIZZZZ
+def myfunction():
+    x = 10
+    print("Value inside function:", x)
+
+x = 20
+myfunction()
+print("Value outside function:", x)
---
+++ mymodule.py
@@ -0,0 +1,8 @@
+#MODULES 
+
+def greeting(name):
+    print("Hello, " + name)
+
+def goodbye(name):
+    print("Goodbye " + name + ", see you later!")
+    
